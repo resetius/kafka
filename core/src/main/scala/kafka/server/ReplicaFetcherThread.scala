6d5096d6a4fa511c21d441dbb5e8f16dc28836ff
Alexey Ozeritsky <aozeritsky@yandex-team.ru>
2015-04-30 23:22:12 +0400

    ReplicaFetcherThread: smart truncation

--- core/src/main/scala/kafka/server/ReplicaFetcherThread.scala
+++ core/src/main/scala/kafka/server/ReplicaFetcherThread.scala
@@ -111,10 +111,16 @@ class ReplicaFetcherThread(name:String,
        * Roll out a new log at the follower with the start offset equal to the current leader's start offset and continue fetching.
        */
       val leaderStartOffset = simpleConsumer.earliestOrLatestOffset(topicAndPartition, OffsetRequest.EarliestTime, brokerConfig.brokerId)
-      warn("Replica %d for partition %s reset its fetch offset from %d to current leader %d's start offset %d"
-        .format(brokerConfig.brokerId, topicAndPartition, replica.logEndOffset.messageOffset, sourceBroker.id, leaderStartOffset))
-      replicaMgr.logManager.truncateFullyAndStartAt(topicAndPartition, leaderStartOffset)
-      leaderStartOffset
+      if (replica.logEndOffset.messageOffset < leaderStartOffset) {
+        warn("Replica %d for partition %s reset its fetch offset from %d to current leader %d's start offset %d"
+          .format(brokerConfig.brokerId, topicAndPartition, replica.logEndOffset.messageOffset, sourceBroker.id, leaderStartOffset))
+        replicaMgr.logManager.truncateFullyAndStartAt(topicAndPartition, leaderStartOffset)
+        leaderStartOffset
+      } else {
+        warn("Replica %d for partition %s reset its fetch offset to %d"
+          .format(brokerConfig.brokerId, topicAndPartition, replica.logEndOffset.messageOffset))
+        replica.logEndOffset.messageOffset
+      }
     }
   }
 
