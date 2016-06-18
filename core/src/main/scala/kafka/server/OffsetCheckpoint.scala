b3dd96b1a9b68003400c331711dac015c06c61a7
Alexey Ozeritsky <aozeritsky@yandex-team.ru>
2016-06-18 15:50:19 +0300

    [LOGBROKER-1637] save 10 versions of checkpoints

--- core/src/main/scala/kafka/server/OffsetCheckpoint.scala
+++ core/src/main/scala/kafka/server/OffsetCheckpoint.scala
@@ -25,6 +25,10 @@ import scala.collection._
 import kafka.utils.Logging
 import kafka.common._
 import java.io._
+import java.nio.channels.FileChannel
+import java.nio.file.Files
+
+import scala.util.Try
 
 object OffsetCheckpoint {
   private val WhiteSpacesPattern = Pattern.compile("\\s+")
@@ -39,12 +43,86 @@ class OffsetCheckpoint(val file: File) extends Logging {
   private val path = file.toPath.toAbsolutePath
   private val tempPath = Paths.get(path.toString + ".tmp")
   private val lock = new Object()
-  file.createNewFile() // in case the file doesn't exist
+  private var version = 0L
+  private val maxVersions = 10
+
+  private val filenameFilter = new FilenameFilter {
+    override def accept(dir: File, name: String): Boolean = {
+      name.startsWith(file.getName)
+    }
+  }
+
+  new File(file + ".tmp").delete() // try to delete any existing temp files for cleanliness
+
+  private implicit val ordering = new Ordering[File] {
+    def compare(a: File, b: File): Int = {
+      val v1 = fileVersion(a)
+      val v2 = fileVersion(b)
+      if (v1 < v2) {
+        -1
+      } else if (v1 == v2) {
+        0
+      } else {
+        1
+      }
+    }
+  }
+
+  private def children = {
+    val list = file.getParentFile.listFiles(filenameFilter)
+    scala.util.Sorting.quickSort(list)
+    list
+  }
+
+  private def currentFile = {
+    val file = children.lastOption.getOrElse(new File(fileName(version)))
+    file.createNewFile()
+    file
+  }
+
+  private def fileName(number: Long): String = {
+    "%s.%x".format(file.getAbsolutePath, number)
+  }
+
+  private def fileVersion(f: File): Long = {
+    try {
+      java.lang.Long.valueOf(f.getName.split('.').last, 16).longValue()
+    } catch {
+      case e: Throwable => 0L
+    }
+  }
+
+  version = children
+    .lastOption
+    .map(fileVersion)
+    .getOrElse(0L)
+
+  for (f <- children.dropRight(maxVersions)) {
+    f.delete()
+  }
+
+  private def syncDir(): Unit = {
+    // fsync dir
+    var dir: FileChannel = null
+    try {
+      dir = FileChannel.open(file.getParentFile.toPath, java.nio.file.StandardOpenOption.READ)
+      dir.force(true)
+    } catch {
+      case e: Throwable =>
+    } finally {
+      if (dir != null) {
+        dir.close()
+      }
+    }
+  }
 
   def write(offsets: Map[TopicAndPartition, Long]) {
     lock synchronized {
       // write to temp file and then swap with the existing file
-      val fileOutputStream = new FileOutputStream(tempPath.toFile)
+      version += 1
+      val currentFile = new File(fileName(version))
+
+      val fileOutputStream = new FileOutputStream(currentFile)
       val writer = new BufferedWriter(new OutputStreamWriter(fileOutputStream))
       try {
         writer.write(CurrentVersion.toString)
@@ -71,15 +149,41 @@ class OffsetCheckpoint(val file: File) extends Logging {
         writer.close()
       }
 
-      Utils.atomicMoveWithFallback(tempPath, path)
+      new File(fileName(version - maxVersions)).delete()
+
+      file.delete()
+      Files.createLink(file.toPath, currentFile.toPath)
+
+      syncDir()
     }
   }
 
+  def malformedLineException(line: String) =
+    new IOException(s"Malformed line in offset checkpoint file: $line'")
+
   def read(): Map[TopicAndPartition, Long] = {
+    var result = Map[TopicAndPartition, Long]()
+    val files = children
+    var idx = files.length - 1
+    var searching = true
+    while (searching && idx >= 0) {
+      try {
+        if (idx != files.length - 1) {
+          warn("try to load previous snapshot: %s".format(files(idx)))
+        }
+        result = readOne(files(idx))
+        searching = result.isEmpty
+      } catch {
+        case e: IOException =>
+          warn("broken snapshot: %s".format(files(idx)), e)
+      }
 
-    def malformedLineException(line: String) =
-      new IOException(s"Malformed line in offset checkpoint file: $line'")
+      idx -= 1
+    }
+    result
+  }
 
+  def readOne(file: File): Map[TopicAndPartition, Long] = {
     lock synchronized {
       val reader = new BufferedReader(new FileReader(file))
       var line: String = null
